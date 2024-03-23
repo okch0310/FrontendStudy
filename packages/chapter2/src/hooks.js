260648efcc128f9de4af7b1d638ce5f658a98903
okch0310 <okch0310@naver.com>
2024-03-23 11:55:18 +0900

    옥채현_B

--- packages/chapter2/src/hooks.js
+++ packages/chapter2/src/hooks.js
@@ -1,14 +1,45 @@
-export function createHooks(callback) {
-  const useState = (initState) => {
-    return [];
+export function createHooks(render) {
+  let states = {};
+  let statesIndex = 0;
+  let memos = {};
+  let memosIndex = 0;
+
+  const useState = (initialState) => {
+    const currentIndex = statesIndex;
+    statesIndex++;
+    if (states[currentIndex] === undefined) {
+      states[currentIndex] = { state: initialState };
+    }
+    const setState = (newState) => {
+      states[currentIndex].state = newState;
+      render();
+    };
+    return [states[currentIndex].state, setState];
   };
 
-  const useMemo = (fn, refs) => {
-    return fn();
+  const useMemo = (factory, deps) => {
+    const currentIndex = memosIndex;
+    memosIndex++;
+    const hasChangedDeps =
+      memos[currentIndex] && !areArraysEqual(memos[currentIndex].deps, deps);
+    if (!memos[currentIndex] || hasChangedDeps) {
+      const value = factory();
+      memos[currentIndex] = { deps, value };
+    }
+    return memos[currentIndex].value;
   };
 
   const resetContext = () => {
+    statesIndex = 0;
+    memosIndex = 0;
+  };
 
+  function areArraysEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
   }
 
   return { useState, useMemo, resetContext };
